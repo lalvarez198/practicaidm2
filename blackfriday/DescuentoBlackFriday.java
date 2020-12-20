91c70e2cdae509b09c2dd52a75e62841b614ef08
JuanIgnacioMagaz <juanchomagaz@gmail.com>
2020-12-20 13:41:24 +0100

    primer commit

---
+++ blackfriday/DescuentoBlackFriday.java
@@ -0,0 +1,14 @@
+import java.lang.IllegalArgumentException;
+
+public class DescuentoBlackFriday {
+// @param precioOriginal es el precio de un producto marcado en la etiqueta
+//porcentajeDescuento es el descuento a aplicar expresado como un porcentaje que ha de ser >= 0
+// @return el precio final teniendo en cuenta que si es black friday (29 de noviembre) se aplica
+//un descuento de porcentajeDescuento
+// @throws IllegalArgumentException si precioOriginal es negativo o porcentajeDescuento es negativo
+
+	public double precioFinal(double precioOriginal, double porcentajeDescuento) throws IllegalArgumentException{
+
+
+	}
+}
