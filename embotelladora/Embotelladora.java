5121d09859228eb3dd04f23cdcfc00b8e28e317a
JuanIgnacioMagaz <juanchomagaz@gmail.com>
2020-12-20 13:16:07 +0100

    añadidos tests

---
+++ embotelladora/Embotelladora.java
@@ -0,0 +1,31 @@
+public class Embotelladora {
+	// @param
+	//pequenas: número de botellas en almacén de 1L
+	// grandes : número de botellas en almacén de 5L
+	//total : número total de litros que hay que embotellar
+
+	// @return
+	//número de botellas pequenas necesarias para embotellar el total de lı́quido, teniendo
+	//en cuenta que hay que minimizar el número de botellas pequenas: primero
+	//se rellenan las grandes
+
+	// @throws NoSolution si no es posible embotellar todo el lı́quido
+
+	public static int calculaBotellasPequenas(int pequenas, int grandes, int total) throws NoSolution {
+
+		if(pequenas < 0 || grandes < 0 || total < 0){
+			String msg = "Numero invalido";
+			throw new NoSolution(msg);
+		}
+		int grandesUsadas = 0;
+		String msg = "Sobra liquido";
+		if(grandes != 0 && total >= 5)
+			grandesUsadas = Math.min((int)Math.floor(total/grandes) , grandes);
+		int litrosRestantes = total - grandesUsadas*5;
+		int pequenasUsadas = Math.min(litrosRestantes, pequenas);
+		litrosRestantes = litrosRestantes - pequenasUsadas;
+		if(litrosRestantes != 0)
+			throw new NoSolution(msg);
+		return pequenasUsadas;
+	}
+}
---
+++ embotelladora/Embotelladora_Tests.java
@@ -0,0 +1,86 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class Embotelladora_Tests {
+	/*Caracterizaciones
+
+	c2: numero nulo de botellas
+		b1: numero nulo de botellas pequenas
+		b2: numero nulo de botellas grande
+	c3: numero suficiente o no de botellas
+		b1: numero suficiente de botellas grandes
+		b2: numero insuficiente de botellas grandes pero suficiente de pequenas
+		b3: numero insuficiente de botellas grandes y pequenas
+	*/
+
+	/*c1 b1*/
+	@Test(expected=NoSolution.class)
+	public void negativeNumLilBottlesTest() throws NoSolution
+        {
+           int pequenas = -5;
+	   int grandes = 2;
+	   int total = 10;
+           assertEquals("Negative Number Little Bottles Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+        }
+
+	/*c1 b2*/
+	@Test(expected=NoSolution.class)
+	public void negativeNumBigBottlesTest() throws NoSolution
+        {
+           int pequenas = 2;
+	   int grandes = -5;
+	   int total = 2;
+           assertEquals("Negative Number Big Bottles Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+        }
+
+	/*c1 b3*/
+	@Test(expected=NoSolution.class)
+	public void negativeTotalNumTest() throws NoSolution
+        {
+           int pequenas = 2;
+	   int grandes = -5;
+	   int total = 2;
+           assertEquals("Negative Total Number Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+        }
+
+	/*c2 b1 y c2 b2*/
+	@Test(expected=NoSolution.class)
+	public void nullNumBottlesTest() throws NoSolution
+        {
+           int pequenas = 0;
+	   int grandes = 0;
+	   int total = 10;
+           assertEquals("Null Number Bottles Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+        }
+
+	/*c3 b1*/
+	@Test public void enoughNumBigBottlesTest() throws NoSolution
+        {
+           int pequenas = 0;
+	   int grandes = 2;
+	   int total = 10;
+           assertEquals("Enough Number Big Bottles Test", 0, Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+        }
+
+	/*c3 b2*/
+	@Test public void enoughNumLilBottlesTest() throws NoSolution
+        {
+           int pequenas = 5;
+	   int grandes = 1;
+	   int total = 10;
+           assertEquals("Enough Number Little Bottles Test", 5, Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+        }
+
+	/*c3 b3*/
+	@Test(expected=NoSolution.class)
+	public void notEnoughBottlesTest() throws NoSolution
+        {
+           int pequenas = 2;
+	   int grandes = 1;
+	   int total = 10;
+           assertEquals("Enough Number Big Bottles Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total));
+        }
+
+
+}
