8e7eac8dd0ec16785471e2ee2c3a0519943be7fc
lalvarez198 <luis.a1998@hotmail.com>
2020-12-20 14:02:33 +0100

    programa principal desarrollado

--- blackfriday/DescuentoBlackFriday.java
+++ blackfriday/DescuentoBlackFriday.java
@@ -1,5 +1,5 @@
 import java.lang.IllegalArgumentException;
-
+import java.util.Calendar;
 public class DescuentoBlackFriday {
 // @param precioOriginal es el precio de un producto marcado en la etiqueta
 //porcentajeDescuento es el descuento a aplicar expresado como un porcentaje que ha de ser >= 0
@@ -7,8 +7,25 @@ public class DescuentoBlackFriday {
 //un descuento de porcentajeDescuento
 // @throws IllegalArgumentException si precioOriginal es negativo o porcentajeDescuento es negativo
 
-	public double precioFinal(double precioOriginal, double porcentajeDescuento) throws IllegalArgumentException{
+	public double precioFinal(double precioOriginal, double porcentajeDescuento, Calendar fecha) throws IllegalArgumentException{
 
+		int mes = fecha.get(Calendar.MONTH);
+		int dia = fecha.get(Calendar.DAY_OF_MONTH);
+		int precioBlackFriday = precioOriginal;
+		if(precioOriginal < 0){
+			throw new IllegalArgumentException();
+		}
+
+		if(porcentajeDescuento < 0 || porcentajeDescuento >100){
+			throw new IllegalArgumentException();
+
+		}
+
+		if(dia == 29 && mes == 11){
+			precioBlackFriday = precioOriginal * (100-porcentajeDescuento)/100);
+		}
+
+		return precioBlackFriday;
 
 	}
 }
